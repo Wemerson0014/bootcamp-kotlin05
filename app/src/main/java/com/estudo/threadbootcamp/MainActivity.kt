12a2a7975e221b5a94016948d141e4bf342e767b
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-04-30 13:16:01 -0300

    Altera visibilidade da progressbar no layout activity_main, cria métodos para exibir ou ocultar a progressbar, cria método para iniciar a task, cria método para exibir o resultado da chamada da api e cria ação do clique do botão na classa MainActivity

--- app/src/main/java/com/estudo/threadbootcamp/MainActivity.kt
+++ app/src/main/java/com/estudo/threadbootcamp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.estudo.threadbootcamp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.estudo.model.AstroPeople
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loadButton = findViewById<Button>(R.id.button_load_data)
+        loadButton.setOnClickListener {
+            launchAstroTask()
+        }
+
+    }
+
+    fun showData(list: List<AstroPeople>?) {
+        val textData = findViewById<TextView>(R.id.text_data)
+        textData.text = ""
+        list?.forEach { people ->
+            textData.append("${people.name} - ${people.craft} \n\n")
+        }
+    }
+
+    fun showLoadingIndicator() {
+        val progressBar = findViewById<ProgressBar>(R.id.progress_main)
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun hideLoadingIndicator() {
+        val progressBar = findViewById<ProgressBar>(R.id.progress_main)
+        progressBar.visibility = View.GONE
+    }
+
+    fun launchAstroTask() {
+        val task = TaskAstro()
+        task.execute()
+    }
+
+    inner class TaskAstro() : AsyncTask<Void, Int, List<AstroPeople>>() {
+
+        private val repository = AstroRepository()
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showLoadingIndicator()
+        }
+
+        override fun doInBackground(vararg params: Void?): List<AstroPeople> {
+            //onProgressUpdate(0)
+            return repository.loadData()
+        }
+
+        override fun onPostExecute(result: List<AstroPeople>?) {
+            super.onPostExecute(result)
+            hideLoadingIndicator()
+            showData(result)
+        }
     }
 }
